a850b50493b34428b77676e497a3ba5adbf6df20
Jared <jared.lee.weiss@gmail.com>
2022-04-25 18:08:00 -0600

    Migrate code to pigpio, cleanup and add comments, get ready for presentation day

--- egen310.py
+++ egen310.py
@@ -1,179 +1,111 @@
 ### EGEN 310R Runner Script
-
-### Resources: 
+### Written by Jared Weiss at Montana State University
+### Resources:
 # Servo control article: https://www.learnrobotics.org/blog/raspberry-pi-servo-motor/
 # YouTube Livestream article: https://www.makeuseof.com/tag/live-stream-youtube-raspberry-pi/
+# pigpio docs: https://docs.juliahub.com/PiGPIO/8aGxa/0.2.0/api/
+# minimize servo jitter: https://ben.akrin.com/raspberry-pi-servo-jitter/
+
+###JOYSTICK CONTROL INFO:
+###Left Joystick: Axis 0, 1
+###    - 1 Up (-1) down (1)
+###    - 0 Left (-1) right (1)
+###Right Joystick: Axis 2, 3
+###    - 3 Up (-1) down (1)
+###    - 2 Left (-1) right (1)
+###Left Trigger: Axis 5
+###    - (-1 no press) (1 full press)
+###Right Trigger: Axis 4
+###A button -> button 0
+###B button -> button 1
+###X -> 3
+###Y -> 4
+###D-Pad:(Hat)Up Down Left Right
+###      (0, 1) (0, -1) (-1, 0) (1, 0)
 
 import pygame
 import RPi.GPIO as GPIO
 from time import sleep
+import pigpio
+import os
+import subprocess
+import platform
 
 def servoSetAngle(servo, angle):
-    #round(angle)
-    duty = angle/18 + 2
-    servo.ChangeDutyCycle(duty)
+    scaled = 500 + angle/180 * 2000 # want to write values between 500 and 2500
+    pwm.set_servo_pulsewidth(servo, scaled)
 
+os.environ["SDL_VIDEODRIVER"] = "dummy"
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
-servo = GPIO.PWM(11, 50) # Send a 50 Hz signal to the servo on pin 11
-servo.start(0)
+# Dictionary of our servo names to pins on the Pi - may be subject to change
+servos = {"drivetrain" : 21, "servo" : 17,
+          "arm_up_down" : 18, "arm_rotate" : 22,
+          "articulation" : 27, "bucket" : 23}
 
-# Define some colors.
-BLACK = pygame.Color('black')
-WHITE = pygame.Color('white')
+# pigpio initialization
+pwm = pigpio.pi() # start up the daemon
+for name, pin in servos.items(): 
+    print(f"Initializing: {name} on pin {pin}")
+    pwm.set_mode(pin, pigpio.OUTPUT) # set each pin to output
+    pwm.set_PWM_frequency(pin, 50)   # with 50 hz signal
 
+# tentative guess for initializing the drivetrain
+servoSetAngle(servos["drivetrain"], 90)
+sleep(2)
 
-# This is a simple class that will help us print to the screen.
-# It has nothing to do with the joysticks, just outputting the
-# information.
-class TextPrint(object):
-    def __init__(self):
-        self.reset()
-        self.font = pygame.font.Font(None, 20)
+pygame.display.init() # initialize a dummy display
+pygame.init()         # initialize the pygame module for controller input
+clock = pygame.time.Clock() # make a clock so we can get accurate timing should we need it
 
-    def tprint(self, screen, textString):
-        textBitmap = self.font.render(textString, True, BLACK)
-        screen.blit(textBitmap, (self.x, self.y))
-        self.y += self.line_height
-
-    def reset(self):
-        self.x = 10
-        self.y = 10
-        self.line_height = 15
-
-    def indent(self):
-        self.x += 10
-
-    def unindent(self):
-        self.x -= 10
-
-
-pygame.init()
-
-# Set the width and height of the screen (width, height).
-#screen = pygame.display.set_mode((500, 700))
-
-#pygame.display.set_caption("My Game")
-
-# Loop until the user clicks the close button.
 done = False
-
-# Used to manage how fast the screen updates.
-clock = pygame.time.Clock()
-
 # Initialize the joysticks.
 pygame.joystick.init()
 
-# Get ready to print.
-textPrint = TextPrint()
-
 # -------- Main Program Loop -----------
 while not done:
-    #
-    # EVENT PROCESSING STEP
-    #
-    # Possible joystick actions: JOYAXISMOTION, JOYBALLMOTION, JOYBUTTONDOWN,
-    # JOYBUTTONUP, JOYHATMOTION
-    #for event in pygame.event.get(): # User did something.
-    #    if event.type == pygame.QUIT: # If user clicked close.
-    #        done = True # Flag that we are done so we exit this loop.
-    #    elif event.type == pygame.JOYBUTTONDOWN:
-    #        print("Joystick button pressed.")
-    #    elif event.type == pygame.JOYBUTTONUP:
-    #        print("Joystick button released.")
-
-    #
-    # DRAWING STEP
-    #
-    # First, clear the screen to white. Don't put other drawing commands
-    # above this, or they will be erased with this command.
-    #screen.fill(WHITE)
-    textPrint.reset()
-
-    # Get count of joysticks.
-    joystick_count = pygame.joystick.get_count()
-
+    # Get rid of events that pygame generates because we
+    # do not care about them at all
+    pygame.event.clear()
+    # Get count of joysticks and stick all the joystick objects
+    # into a cute little list 
+    joysticks = [pygame.joystick.Joystick(x) for x in range(pygame.joystick.get_count())]
     # For each joystick:
-    for i in range(joystick_count):
-        joystick = pygame.joystick.Joystick(i)
-        joystick.init()
-
-        # Usually axis run in pairs, up/down for one, and left/right for
-        # the other.
+    for joystick in joysticks:
+        print(joystick.get_name()) # log debug info to the console
         axes = joystick.get_numaxes()
-        #textPrint.tprint(screen, "Number of axes: {}".format(axes))
-        #textPrint.indent()
-
-        for i in range(axes):
+        for i in range(axes): # do different stuff with each one
             axis = joystick.get_axis(i)
-            if (i == 0):
-                servoSetAngle(servo, (axis+1)*90)
-            textPrint.tprint(screen, "Axis {} value: {:>6.3f}".format(i, axis))
-        #textPrint.unindent()
+            if (i == 0): 
+                servoSetAngle(servos["servo"]       , (axis+1)*90) # servo gos through full range of motion
+            if (i == 1):
+                servoSetAngle(servos["drivetrain"]  , (axis+1)*90) # drivetrain probably needs to be toned down
+            if (i == 2):
+                servoSetAngle(servos["arm_rotate"]  , (axis+1)*15) # arm rotation may need to be toned up as we stabilize it
+            if (i == 3):
+                servoSetAngle(servos["arm_up_down"] , (axis+1)*90) # arm up and down full range
+            if (i == 4):
+                servoSetAngle(servos["articulation"], (axis+1)*60) # articulation (second servo on arm) with diminished range
+            if (i == 5):
+                servoSetAngle(servos["bucket"]      , -1*(axis+1)*45+90) # bucket servo
+            print("Axis {} value: {:>6.3f}".format(i, axis))
 
         buttons = joystick.get_numbuttons()
-        #textPrint.tprint(screen, "Number of buttons: {}".format(buttons))
-        #textPrint.indent()
+        print("Number of buttons: {}".format(buttons))
 
+        # Exit on the B button
         for i in range(buttons):
             button = joystick.get_button(i)
             if (i == 1):
                 if (button):
                     done = True
-            #textPrint.tprint(screen,
-            #                 "Button {:>2} value: {}".format(i, button))
-        #textPrint.unindent()
+            print("Button {:>2} value: {}".format(i, button))
+    
+    clock.tick(20)
 
-        hats = joystick.get_numhats()
-        #textPrint.tprint(screen, "Number of hats: {}".format(hats))
-        #textPrint.indent()
-
-        # Hat position. All or nothing for direction, not a float like
-        # get_axis(). Position is a tuple of int values (x, y).
-        for i in range(hats):
-            hat = joystick.get_hat(i)
-            #textPrint.tprint(screen, "Hat {} value: {}".format(i, str(hat)))
-        #textPrint.unindent()
-
-        #textPrint.unindent()
-
-    #
-    # ALL CODE TO DRAW SHOULD GO ABOVE THIS COMMENT
-    #
-
-    # Go ahead and update the screen with what we've drawn.
-    #pygame.display.flip()
-
-    # Limit to 20 frames per second.
-    #clock.tick(20)
-
-# Close the window and quit.
-# If you forget this line, the program will 'hang'
-# on exit if running from IDLE.
-
-"""
-JOYSTICK CONTROL INFO:
-Left Joystick: Axis 0, 1
-    - 1 Up (-1) down (1)
-    - 0 Left (-1) right (1)
-Right Joystick: Axis 2, 3
-    - 3 Up (-1) down (1)
-    - 2 Left (-1) right (1)
-Left Trigger: Axis 5
-    - (-1 no press) (1 full press)
-Right Trigger: Axis 4
-A button -> button 0
-B button -> button 1
-X -> 3
-Y -> 4
-
-D-Pad:(Hat)Up Down Left Right
-      (0, 1) (0, -1) (-1, 0) (1, 0)
-"""
-
-# Constants will have to change as we figure out duty for our servos
 pygame.quit()
-servo.stop() # stop the servo
-GPIO.cleanup() # general cleanup
+for name, pin in servos.items():
+    print(f"Cleaning up {name} on pin {pin}")
+    pwm.set_PWM_dutycycle(pin, 0)
+    pwm.set_PWM_frequency(servo, 0)
+    
+# End egen310.py
